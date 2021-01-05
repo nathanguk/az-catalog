851595770e26538897d8a806d02210b25625c8ff
Nathan Gaskill <nathangaskill@hotmail.com>
2021-01-05 11:25:37 +0000

    Update

--- getTemplates/index.js
+++ getTemplates/index.js
@@ -4,14 +4,16 @@ module.exports = async function (context, req) {
     context.log(req.query);
     const account = process.env.GITHUB_ACCOUNT;
     const repo = process.env.GITHUB_REPO;
+    const folders = process.env.GITHUB_EXCLUDE;
+    const excludedFolders = folders.split(",");
 
-    let response = await fetch(`https://api.github.com/repos/${account}/${repo}/contents/`);
+    const response = await fetch(`https://api.github.com/repos/${account}/${repo}/contents/`);
     const gitContents = await response.json();
 
     let templates = [];
 
     gitContents.filter(function (template) {
-        if(template.type == "dir" && template.name != ".github"){
+        if(template.type == "dir" && !excludedFolders.includes(template.name)){
             context.log(template.path);
             templates.push(template.path);
             return
