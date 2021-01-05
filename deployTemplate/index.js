178593b53a363001e8cac2578f595b34cc1da116
Nathan Gaskill <nathangaskill@hotmail.com>
2021-01-05 14:09:30 +0000

    Update

--- deployTemplate/index.js
+++ deployTemplate/index.js
@@ -1,10 +1,10 @@
 module.exports = async function (context, req) {
-    context.log(req.query);
+    context.log(req.body);
     const account = process.env.GITHUB_ACCOUNT;
     const repo = process.env.GITHUB_REPO;
 
     let deployUri = "https://portal.azure.com/#create/Microsoft.Template/uri/";
-    let templateUri = encodeURIComponent(`https://raw.githubusercontent.com/${account}/${repo}/master/${req.query.template}/azuredeploy.json`);
+    let templateUri = encodeURIComponent(`https://raw.githubusercontent.com/${account}/${repo}/master/${req.body.template}/azuredeploy.json`);
     let location = deployUri + templateUri;
 
     context.res = {
