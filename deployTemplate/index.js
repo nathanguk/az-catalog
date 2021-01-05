975647bf52c2d90099f39b25d1274658297c50d6
Nathan Gaskill <nathangaskill@hotmail.com>
2021-01-05 08:02:07 +0000

    Initial Commit

---
+++ deployTemplate/index.js
@@ -0,0 +1,13 @@
+module.exports = async function (context, req) {
+    context.log(req.query);
+    const account = process.env.GITHUB_ACCOUNT;
+    const repo = process.env.GITHUB_REPO;
+
+    let deployUri = "https://portal.azure.com/#create/Microsoft.Template/uri/";
+    let templateUri = encodeURIComponent(`https://raw.githubusercontent.com/${account}/${repo}/master/${req.query.template}/azuredeploy.json`);
+    let location = deployUri + templateUri;
+
+    context.res = {
+        status: 302, headers: { "location": location }
+    };
+}
---
+++ getTemplates/index.js
@@ -0,0 +1,22 @@
+const fetch = require('node-fetch');
+
+module.exports = async function (context, req) {
+    context.log(req.query);
+    const account = process.env.GITHUB_ACCOUNT;
+    const repo = process.env.GITHUB_REPO;
+
+    let response = await fetch(`https://api.github.com/repos/${account}/${repo}/contents/`);
+    const gitContents = await response.json();
+
+    context.log(gitContents);
+
+    let templates = gitContents.filter(function (template) {
+        return template.type == "dir"
+    });
+
+
+    context.res = {
+        status: 200,
+        body: templates
+    };
+}
