930a1335834c0e9b5fd007e8e6d165e12479814d
Nathan Gaskill <nathangaskill@hotmail.com>
2021-01-05 13:16:02 +0000

    Update

--- getTemplates/index.js
+++ getTemplates/index.js
@@ -1,28 +1,41 @@
 const fetch = require('node-fetch');
 
 module.exports = async function (context, req) {
-    context.log(req.query);
-    const account = process.env.GITHUB_ACCOUNT;
-    const repo = process.env.GITHUB_REPO;
-    const folders = process.env.GITHUB_EXCLUDE;
-    const excludedFolders = folders.split(",");
 
-    const response = await fetch(`https://api.github.com/repos/${account}/${repo}/contents/`);
-    const gitContents = await response.json();
+    try{
 
-    let templates = [];
-
-    gitContents.filter(function (template) {
-        if(template.type == "dir" && !excludedFolders.includes(template.name)){
-            context.log(template.path);
-            templates.push(template.path);
-            return
+        const account = process.env.GITHUB_ACCOUNT;
+        const repo = process.env.GITHUB_REPO;
+        const folders = process.env.GITHUB_EXCLUDE;
+        const excludedFolders = folders.split(",");
+    
+        const response = await fetch(`https://api.github.com/repos/${account}/${repo}/contents/`);
+        const gitContents = await response.json();
+    
+        let templates = [];
+    
+        gitContents.filter(function (template) {
+            if(template.type == "dir" && !excludedFolders.includes(template.name)){
+                context.log(template.path);
+                templates.push(template.path);
+                return
+            };
+        });
+    
+        context.res = {
+            status: 200,
+            body: templates
         };
-    });
 
+    }catch(err){
 
-    context.res = {
-        status: 200,
-        body: templates
+        context.log(err.message);
+        
+        context.res = {
+            status: 500,
+            body: "Internal Server Error"
+        }; 
+
     };
-}
+
+};
